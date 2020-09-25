5aa2c41e4658482297ed4f180fdbad7a5320d9dc
Danilo Meuser <danilo.meuser@rwth-aachen.de>
2020-09-25 13:41:37 +0200

    Add pede option to ignore constraints for inactive parameters

--- Alignment/CommonAlignmentProducer/python/AlcaSiPixelAliHarvester_cff.py
+++ Alignment/CommonAlignmentProducer/python/AlcaSiPixelAliHarvester_cff.py
@@ -28,6 +28,9 @@ SiPixelAliPedeAlignmentProducer.ParameterBuilder.Selector = cms.PSet(
 #################################################################
 from Configuration.ProcessModifiers.high_granularity_pcl_cff import high_granularity_pcl
 high_granularity_pcl.toModify(SiPixelAliPedeAlignmentProducer.ParameterBuilder.Selector,alignParams=cms.vstring("TrackerP1PXBLadder,111111","TrackerP1PXECPanel,111111"))
+#  ~high_granularity_pcl.toModify(SiPixelAliPedeAlignmentProducer.ParameterBuilder.Selector,alignParams=cms.vstring("TrackerP1PXBLadder,111fff","TrackerP1PXECPanel,111fff"))
+#  ~high_granularity_pcl.toModify(SiPixelAliPedeAlignmentProducer.ParameterBuilder.Selector,alignParams=cms.vstring("PixelHalfBarrels,111111","PXECHalfCylinders,111111","TrackerP1PXBLadder,111fff","TrackerP1PXECPanel,111fff"))
+#  ~high_granularity_pcl.toModify(SiPixelAliPedeAlignmentProducer.ParameterBuilder.Selector,alignParams=cms.vstring("TrackerP1PXBLadder,111fff","TrackerP1PXECPanel,111fff","TrackerTIBHalfBarrel,111111","TrackerTOBHalfBarrel,111111","TrackerTIDEndcap,111110","TrackerTECEndcap,111110"))
 
 SiPixelAliPedeAlignmentProducer.doMisalignmentScenario = False #True
 
@@ -51,7 +54,8 @@ SiPixelAliPedeAlignmentProducer.algoConfig.pedeSteerer.options = cms.vstring(
      'entries 500',
      'chisqcut  30.0  4.5',
      'threads 1 1',
-     'closeandreopen'
+     'closeandreopen',
+     'skipemptycons'    #ignore constraints for inactive alignment parameters
      #'outlierdownweighting 3','dwfractioncut 0.1'
      #'outlierdownweighting 5','dwfractioncut 0.2'
     )
